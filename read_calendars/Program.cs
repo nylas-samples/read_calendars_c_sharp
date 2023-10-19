99b5a61bcefe8c6e526c1b8a2bdf98fa5c58f7ca
Blag <atejada@gmail.com>
2023-10-18 22:42:42 -0400

    Update Program.cs

--- read_calendars/Program.cs
+++ read_calendars/Program.cs
@@ -1,23 +1,34 @@
 // Import packages
 using RestSharp;
-using System;
+using System.Text.Json;
 
 // Load .env file
 DotNetEnv.Env.Load();
 DotNetEnv.Env.TraversePath().Load();
 
 // Create a new Rest Client and call the Nylas API endpoint
-var client = new RestSharp.RestClient("https://api.nylas.com/calendars");
-// Wait until the connection is done
-client.Timeout = -1;
+var client = new RestSharp.RestClient("https://api.us.nylas.com/v3/grants/" +
+             Environment.GetEnvironmentVariable("GRANT_ID") + "/calendars");
 // We want a GET method
-var request = new RestSharp.RestRequest(Method.GET);
+var request = new RestRequest();
 // Adding header and authorization
 request.AddHeader("Content-Type", "application/json");
-request.AddHeader("Authorization", "Bearer " + System.Environment.GetEnvironmentVariable("ACCESS_TOKEN"));
+request.AddHeader("Authorization", "Bearer " +
+                  Environment.GetEnvironmentVariable("API_KEY_V3"));
 // We send the request
-IRestResponse response = client.Execute(request);
-// Print the response
-Console.WriteLine(response.Content);
+RestResponse response = client.Execute(request);
+// Parse the content as JSON
+var content = JsonDocument.Parse(response.Content);
+var data = content.RootElement.GetProperty("data");
+
+Console.WriteLine(data);
+
+foreach(var calendar in data.EnumerateArray())
+{
+    //Console.WriteLine(calendar.GetProperty("id"));
+    Console.WriteLine("Id: {0} / Name: {1}",
+        calendar.GetProperty("id"), calendar.GetProperty("name"));
+    Console.WriteLine("");
+}
 // Wait for user input before closing the terminal
 Console.Read();
